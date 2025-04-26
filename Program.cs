1fd7a6382328d3fa6fce5bcba8bb4625f38fbe29
Ronald C Milligan <ronald.c.milligan6.civ@us.navy.mil>
2025-04-26 14:53:19 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+
+
+using System.Diagnostics;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Beginning program. Running Sql Script to build database tables for your data pull");
+        SqlHelper helper = new SqlHelper();
+        helper.Helper("Server=localhost;Database=vids_db;User ID=root;Password=password;", "C:\\Users\\developer1\\Desktop\\Dynamic\\dot-net\\sqlhelper\\SqlHelper\\dump.sql");
+        
+    }
+}
---
+++ SqlHelper.cs
@@ -0,0 +1,43 @@
+﻿using MySql.Data;
+using MySql.Data.MySqlClient;
+using System;
+using System.Linq.Expressions;
+
+
+public class SqlHelper
+{
+    private string sqlconnectionstring;
+	private string sqlscriptpath;
+	
+
+	public void Helper(string connectionstring, string scriptpath)
+	{
+		//Pass in connection string for mysql database, and path to your sql script to run
+		sqlconnectionstring = connectionstring;
+		sqlscriptpath = scriptpath;
+
+		//Read sql script
+		string sql = File.ReadAllText(sqlscriptpath);
+
+		//Create connection to database
+		var connection = new MySqlConnection(connectionstring);
+
+		//Create Mysql commands from script
+		var sqlcommandsfromscript = new MySqlCommand(sql, connection);
+
+
+		//Open connection and execute sql script
+		try { 
+			connection.Open();
+            Console.WriteLine(sqlcommandsfromscript.ExecuteNonQueryAsync());
+            connection.Close();
+			Console.WriteLine("Script Executed successfully");
+			Console.WriteLine("Press Enter to exit........");
+            Console.ReadLine();
+			
+        } catch (MySqlException e) { 
+			Console.WriteLine(e.Message);
+		}
+		
+    }
+}
