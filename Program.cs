783dfea2a1f607a54b0b73d178bf43cf775daf77
Ronald Milligan <ronmilligan133@gmail.com>
2025-04-26 14:57:53 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ class Program
     {
         Console.WriteLine("Beginning program. Running Sql Script to build database tables for your data pull");
         SqlHelper helper = new SqlHelper();
-        helper.Helper("Server=localhost;Database=vids_db;User ID=root;Password=password;", "C:\\Users\\developer1\\Desktop\\Dynamic\\dot-net\\sqlhelper\\SqlHelper\\dump.sql");
+        helper.Helper("Server=localhost;Database=_db;User ID=root;Password=password;", "C:\\Users\\developer1\\Desktop\\Dynamic\\dot-net\\sqlhelper\\SqlHelper\\dump.sql");
         
     }
 }
